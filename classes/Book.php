3c5505c0524c7d35f3f573d0cb0076247f374335
bpeg <bpegirk@ya.ru>
2018-04-21 13:54:56 +0800

    * remove guzzle * bool draw split by year/month

--- classes/Book.php
+++ classes/Book.php
@@ -2,8 +2,6 @@
 
 namespace classess;
 
-use GuzzleHttp\Client;
-
 
 /**
  * Class Book
--- index.php
+++ index.php
@@ -2,8 +2,16 @@
 // list of the books
 $f = scandir('books');
 
+$books = [];
 foreach ($f as $file) {
+
     if (strstr($file, 'html')) {
-        echo '<a href="books/' . $file . '">' . substr($file, 0, -4) . '</a>';
+        $y = date('Y-m', filemtime('books/' . $file));
+        $books[$y][] = '<p><a href="books/' . $file . '">' . substr($file, 0, -4) . '</a></p>';
     }
+}
+
+foreach ($books as $year => $html) {
+    echo "<h1>$year</h1>" . implode('', $html);
+
 }
